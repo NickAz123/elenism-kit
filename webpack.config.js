a457a0cf07a3e6690621e6dfe510d417e039f9b0
Nick <nick.azurin@gmail.com>
2024-10-31 20:04:47 -0700

    added pack necessary packages and folders

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./index.js",
+  output: {
+    path: path.resolve("dist"),
+    filename: "index.js",
+    library: "yourLibraryName",
+    libraryTarget: "umd",
+  },
+  externals: {
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "React",
+      root: "React",
+    },
+    "react-dom": {
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+      amd: "ReactDOM",
+      root: "ReactDOM",
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.css$/, // Handle CSS files
+        use: [
+          "css-loader", // Interpret @import and url() like import/require()
+          "postcss-loader", // Process CSS with PostCSS
+          "style-loader",
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  externals: {
+    react: "react",
+  },
+};
