a7eb201853f1fdd770293ce765ac14e9ed41bd69
Nick Az <nick.azurin@gmail.com>
2024-11-28 20:36:45 -0800

    added a testing-app section in the project for locally testing components

---
+++ testing-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import TestComponent  from './components/testComponent';
+import LandingPage from './components/Landing/landing';
+
+function App() {
+  return (
+    <div className="App">
+     <TestComponent />
+     <LandingPage />
+    </div>
+  );
+}
+
+export default App;
