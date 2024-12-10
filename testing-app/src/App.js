1e10616ed45c1877545a9400c2b1ed1d498c26d3
Nick Az <nick.azurin@gmail.com>
2024-12-09 22:09:20 -0800

    Added .env file processing for testing application firebase keys. Tested initialization from test app

--- testing-app/src/App.js
+++ testing-app/src/App.js
@@ -1,7 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 import TestComponent  from './components/testComponent';
 import LandingPage from './components/Landing/landing';
+
+import { firebaseInit, getFirebaseApp } from './js/firebase/firebaseConfig.js';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID
+};
+
+const { firebaseApp, firestore, auth } = firebaseInit(firebaseConfig);
 
 function App() {
   return (
