0a1ed1e20ba7182615f42ed7357532c50af519e9
Nick Az <nick.azurin@gmail.com>
2025-01-12 22:03:10 -0800

    added the new components to the master testing app folder

--- testing-app/src/App.js
+++ testing-app/src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 import TestComponent  from './components/testComponent';
 import LandingPage from './components/Landing/landing';
+import RSVP from './components/RSVP/rsvp';
+import Schedule from './components/Schedule/schedule'
 
 function App() {
   return (
     <div className="App">
-     <TestComponent />
      <LandingPage />
+     <RSVP/>
+     <Schedule/>
     </div>
   );
 }
